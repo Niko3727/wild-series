41469ffe0244d59dc5100c0ac9e4498aad4f79f5
Niko3727 <niko__@live.fr>
2024-05-27 17:32:23 +0200

    0.08

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,48 @@
+<?php 
+
+namespace App\Controller; 
+
+ 
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route; //Cette ligne permet d'afficher la route. 
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+
+#[Route('/category', name: 'category_')]
+class CategoryController extends AbstractController
+{
+    #[Route('/', name: 'category_index')]
+    public function index(CategoryRepository $categoryRepository): Response 
+    {
+        $categorys = $categoryRepository->findAll();
+        
+        return $this->render ('category/index.html.twig', ['categorys' => $categorys]
+        );
+
+    }
+    #[Route('/{categoryName}', name: 'show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository): Response
+    {
+        $category = $categoryRepository->findOneBy(['name' => $categoryName]);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'Aucune catégorie nommée ' . $categoryName .' est disponible.'
+            );
+        }
+
+        $programs = $programRepository->findBy(['category' => $category],['id' => 'DESC'], 3);
+
+        if (!$programs) {
+            throw $this->createNotFoundException(
+                'No category with'.$categoryName.' found in category\'s table.'
+            );
+        }
+
+        return $this->render ('category/show.html.twig', ['category' => $category, 'programs' => $programs]
+        );
+
+    }
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,27 +2,37 @@
 
 namespace App\Controller; 
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route; //Cette ligne permet d'afficher la route. 
+use App\Repository\ProgramRepository;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response 
+    public function index(ProgramRepository $programRepository): Response 
     {
+        $programs = $programRepository->findAll();
         
-        return $this->render ('program/index.html.twig', ['website' => 'Wild Series']
+        return $this->render ('program/index.html.twig', ['programs' => $programs]
         );
 
     }
 
     #[Route('/{id}', methods:['GET'], requirements: ['id'=>'\d+'], name: 'show')]
-    public function show(int $id): Response
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
         
-        return $this->render ('program/show.html.twig', ['id' => $id]
+        return $this->render ('program/show.html.twig', ['program' => $program,]
         );
 
     }
